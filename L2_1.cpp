5e673ed54a53f8927ba48b4d65f6d4149b30001d
sasuke <stqnguyen@ucdavis.edu>
2022-03-18 16:24:41 -0700

    finish L2_1

--- L2_1.cpp
+++ L2_1.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <math.h>
 #include <iomanip>
@@ -7,52 +6,43 @@ using namespace std;
 int main()
 {
     int cookies, leftCookies, boxes, containers, leftContainers;
+
     cout << "Enter the number of cookies perbox or press" << endl;
-    cout << "'Enter' to use the default of 24 cookies per box.";
-    cout << endl
-         << endl;
-    cout << "Default cookies per box will be used." << endl;
-    cout << endl
-         << endl;
-
-    cout << "Enter the number of boxes per container or press" << endl;
-    cout << " 'Enter' to use the default of 75 boxes per container." << endl;
-    cout << endl
-         << endl;
-    cout << "Default boxes percontainer will be used." << endl;
-    cout << endl
-         << endl;
-
-    cout << " Enter the total number of cookies: ";
+    cout << " 'Enter' to use the default of 24 cookies per box." << endl;
+    cout << "\nDefault cookies per box will be used." << endl;
+    cout << "\nEnter the number of boxes per container or press" << endl;
+    cout << "  'Enter' to use the default of 75 boxes per container." << endl;
+    cout << "\nDefault boxes percontainer will be used." << endl;
+    cout << "\nEnter the total number of cookies: ";
     cin >> cookies;
 
-    if (cookies > 0)
+    if (!(cookies > 0))
     {
-        cout << "\nThe total number of cookies is: " << cookies;
+        cout << "You entered the wrong value, please try it again" << endl;
+        return 1;
     }
     else
     {
-        cout << "you didn't enter value yet, please try it again";
+        boxes = cookies / 24;
+        leftCookies = cookies % 24;
+        containers = boxes / 75;
+        leftContainers = boxes % 75;
+
+        if (boxes > 0)
+            cout << "\n"
+                 << setw(59) << "The number of cookies boxes needed to hold the cookies: " << boxes << endl;
+
+        if (leftCookies > 0)
+        {
+            cout << setw(59) << "Left Cookie: " << leftCookies << endl;
+        }
+
+        if (containers > 0)
+            cout << "The number of containers needed to store the cookie boxes: " << containers << endl;
+
+        if (leftContainers > 0)
+        {
+            cout << setw(59) << "Leftover boxes: " << leftContainers << endl;
+        }
     }
-
-    boxes = cookies / 24;
-    leftCookies = cookies % 24;
-    containers = boxes / 75;
-    leftContainers = boxes % 75;
-
-    if (boxes > 0)
-        cout << "\nThe number of cookies boxes needed to hold the cookies: " << boxes << endl;
-
-    if (leftCookies > 0) {
-        std::cout << std::setw(59);
-        std::cout << "leftCookie: " << leftCookies <<  endl;}
-        
-
-    if (containers > 0)
-        cout << "\nThe number of containers needed to store the cookie boxes: " << containers << endl;
-
-    if (leftContainers > 0) {
-        std::cout << std::setw(59);
-        std::cout << "Leftover boxes: " << leftContainers <<  endl;}
-    return 0;
 }
