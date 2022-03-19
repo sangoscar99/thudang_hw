62c940ca63e668d9a4303ca0224d5a87a36b8961
sasuke <stqnguyen@ucdavis.edu>
2022-03-18 17:25:31 -0700

    finish part2

--- L2_2.cpp
+++ L2_2.cpp
@@ -1,107 +1,90 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+int main()
 {
-double area, fSeed, fPlantation, fFertilizing, fLabor, fTotal, fSellingprice, totalReveneu, proFit;
-double area1,area2, sSeed, sPlantation, sFertilizing, sLabor, sTotal, sSellingprice;
- 
-int number;
+    double area, fSeed, fPlantation, fFertilizing, fLabor, fTotal, fSellingprice, totalReveneu, proFit;
+    double area1, area2, sSeed, sPlantation, sFertilizing, sLabor, sTotal, sSellingprice, per1;
+    int number;
 
-cout << " Enter farm area in acress: ";
-cin>> area;
+    cout << "Enter farm area in acress: ";
+    cin >> area;
 
-cout << " Enter the number of vegetables planted(1/2): ";
-cin >> number;
+    cout << "\nEnter the number of vegetables planted(1/2): ";
+    cin >> number;
 
-// 1 or 2
+    if (number == 1)
+    {
+        cout << "\nEnter the vegetable seed cost per acre: $" << endl;
+        cin >> fSeed;
 
-if (number==1)
-{
-    cout << "Enter the vegetable seed cost per acre: $"<< endl;
-    cin >> fSeed;
-     
-    cout << "Enter the vegetable plantation cost per acre: $" <<endl;
-    cin >> fPlantation;
+        cout << "\nEnter the vegetable plantation cost per acre: $" << endl;
+        cin >> fPlantation;
 
-    cout <<"Enter the vegetable fertilizing cost per acre: $" <<endl;
-    cin >> fFertilizing;
+        cout << "\nEnter the vegetable fertilizing cost per acre: $" << endl;
+        cin >> fFertilizing;
 
-    cout << "Enter the vegetable labor cost per acre: $" << endl;
-    cin >> fLabor;
+        cout << "\nEnter the vegetable labor cost per acre: $" << endl;
+        cin >> fLabor;
 
-    fTotal = fSeed + fPlantation + fFertilizing + fLabor;
-     cout <<" Enter vegetable selling price per acre: $";
-     cin >> fSellingprice;
+        fTotal = fSeed + fPlantation + fFertilizing + fLabor;
+        cout << "\nEnter vegetable selling price per acre: $";
+        cin >> fSellingprice;
 
-     totalReveneu = fSellingprice*area;
-     proFit = totalReveneu- (fTotal*area);
+        totalReveneu = fSellingprice * area;
+        proFit = totalReveneu - (fTotal * area);
+    }
+    else if (number == 2)
+    {
+        cout << "\nEnter the portion (as a percentage) of land used for the first vegetable: ";
+        cin >> per1;
+        area1 = per1 * 0.01 * area;
+        area2 = area - area1;
 
-}
-else if (number ==2)
-{ 
- cout << "Enter the portion (as a percentage) of land used for the first vegetable: " << endl;
- cin >> area2;
- area2 = 100 - area1;
+        cout << "\nEnter the first vegetable seed cost per acre: $";
+        cin >> fSeed;
 
+        cout << "\nEnter the first vegetable plantation cost per acre: $";
+        cin >> fPlantation;
 
- //cout << "Enter the portion (as a percentage) of land used for the first vegetable: " << endl;
- //cin >> area2;
+        cout << "\nEnter the first vegetable fertilizing cost per acre: $";
+        cin >> fFertilizing;
 
- cout << "Enter the first vegetable seed cost per acre: $"<< endl;
-    cin >> fSeed;
-     
-    cout << "Enter the first vegetable plantation cost per acre: $" <<endl;
-    cin >> fPlantation;
+        cout << "\nEnter the first vegetable labor cost per acre: $";
+        cin >> fLabor;
 
-    cout <<"Enter the first vegetable fertilizing cost per acre: $" <<endl;
-    cin >> fFertilizing;
+        fTotal = fSeed + fPlantation + fFertilizing + fLabor;
+        cout << "\nEnter vegetable selling price per acre: $";
+        cin >> fSellingprice;
 
-    cout << "Enter the first vegetable labor cost per acre: $" << endl;
-    cin >> fLabor;
+        cout << "\nEnter the second vegetable seed cost per acre: $";
+        cin >> sSeed;
 
-    fTotal = fSeed + fPlantation + fFertilizing + fLabor;
-     cout <<" Enter vegetable selling price per acre: $";
-     cin >> fSellingprice;
+        cout << "\nEnter the second vegetable plantation cost per acre: $";
+        cin >> sPlantation;
 
-     
- cout << "Enter the second vegetable seed cost per acre: $"<< endl;
-    cin >> sSeed;
-     
-    cout << "Enter the second vegetable plantation cost per acre: $" <<endl;
-    cin >> sPlantation;
+        cout << "\nEnter the second vegetable fertilizing cost per acre: $";
+        cin >> sFertilizing;
 
-    cout <<"Enter the second vegetable fertilizing cost per acre: $" <<endl;
-    cin >> sFertilizing;
+        cout << "\nEnter the second vegetable labor cost per acre: $";
+        cin >> sLabor;
 
-    cout << "Enter the second vegetable labor cost per acre: $" << endl;
-    cin >> sLabor;
+        sTotal = sSeed + sPlantation + sFertilizing + sLabor;
+        cout << "\nEnter the second vegetable selling cost per acre: $";
+        cin >> sSellingprice;
 
-    sTotal = sSeed+ sPlantation+ sFertilizing+ sLabor;
-    cout << "Enter the second vegetable selling cost per acre: $" << endl;
-    cin >> sSellingprice;
+        totalReveneu = (area1 * fSellingprice) + (area2 * sSellingprice);
+        proFit = totalReveneu - (fTotal* area1 + sTotal * area2);
+    }
 
-   totalReveneu = (area* 0.01 * area1 * fSellingprice) + (area * 0.01 * area2* sSellingprice);
-   
-   proFit = totalReveneu-(fTotal+sTotal);
-}
+    else
+    {
+        // error handling
+        cout << "\nYou entered the wrong input or you didnt, pls try again";
+        return 1;
+    }
 
-   else 
-   {
-//errrrrr
-
-cout <<"you entered the wrong input or you didnt, pls try again";
-
-return 1;
-   }
-
-   // final
-   cout << " The total revenue is $:" <<totalReveneu << endl;
-   cout <<" Profit/loss is $: " << proFit <<endl;
-
-   return 0;
-
-
-
-
+    // final statement
+    cout << "\nThe total revenue is : $ " << totalReveneu << endl;
+    cout << "Profit/loss is $: " << proFit << endl;
 }
